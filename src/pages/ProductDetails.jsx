f11b4363ee1b20d4c0aafa46c11b06a2aec59ba5
denisko <denis374@gmail.com>
2022-08-29 18:36:53 +0300

    страница с полным отображением товара и отображение схожих продуктов на странице описания товара(стрелка возврата)

--- src/pages/ProductDetails.jsx
+++ src/pages/ProductDetails.jsx
@@ -1,10 +1,142 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
+import products from "../assets/fake-data/products";
+import CommonSection from "../components/common-section/CommonSection";
+import {Col, Container, Row} from "react-bootstrap";
+import product_01_image_01 from "../assets/images/product_01_image_01.jpg";
+
+import '../styles/productDetails.css';
+import '../styles/allProducts.css';
+import '../styles/product-cart.css';
+import {useNavigate, useParams} from "react-router-dom";
+import ProductCart from "../components/product-cart/ProductCart";
 
 const ProductDetails = () => {
+    //отображение картинки продукта
+    const {id} = useParams()
+    const product = products.find(item => item.id === id)
+    const [prevImg, setPrevImg] = useState(product.image01)
+
+    const { title, price, category, desc, image01 } = product;
+
+    //переключения описания и отзывов
+    const [tab, setTab] = useState("desc");
+    //схожие товары
+    const relatedProduct = products.filter((item) => category === item.category);
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        setPrevImg(product.image01)
+    },[product])
+
+    const goBack = () => {
+        navigate(-1)
+    }
+
+
     return (
-        <div>
-            Product
+    <div>
+        <div className='comm-sec'>
+            <CommonSection title={title}/>
         </div>
+        <section>
+            <Container>
+                <button className="back__btn mb-5" onClick={goBack}>
+                    <i className="ri-arrow-go-back-line "></i>
+                </button>
+                <Row>
+                    <Col lg="2" md="2">
+                        <div className="product__images ">
+                            <div className="img__item mb-3" onClick={() => setPrevImg(product.image01)}>
+                                <img src={product.image01} alt="" className="w-25"/>
+                            </div>
+                            <div className="img__item mb-3" onClick={() => setPrevImg(product.image02)}>
+                                <img src={product.image02} alt="" className="w-25"/>
+                            </div>
+                            <div className="img__item" onClick={() => setPrevImg(product.image03)}>
+                                <img src={product.image03} alt="" className="w-25"/>
+                            </div>
+                        </div>
+                    </Col>
+                    <Col lg="4" md="4">
+                        <div className="product__main-img">
+                            <img src={prevImg} alt="" className="w-80"/>
+                        </div>
+                    </Col>
+
+                    <Col lg="6" md="6">
+                        <div className="single__product-content">
+                            <h2 className="product__title mb-3">{title}</h2>
+                            <p className="product__price">
+                                Price: <span>${price}</span>
+                            </p>
+                            <p className="category mb-5">
+                                Category: <span>{category}</span>
+                            </p>
+                            <button className="addCart__btn">
+                                Добавить
+                            </button>
+                        </div>
+                    </Col>
+
+                    <Col lg="12">
+                        <div className="tabs d-flex align-items-center gap-5 py-3">
+                            <h6 className={`${tab === 'desc' ? 'tab__active': ''}`} onClick={() => setTab('desc')}>Описание</h6>
+                            <h6 className={`${tab === 'rev' ? 'tab__active': ''}`} onClick={() => setTab('rev')}>Отзыв</h6>
+                        </div>
+                        {
+                            tab === 'desc' ?
+                                <div className="tab__content">
+                                    <p>{desc}</p>
+                                </div> :
+                                <div className="tab__form">
+                                    <div className="review pt-4">
+                                        <p className="user__name mb-0">Jho Doe</p>
+                                        <p className="user__email">jhon1@gmail.com</p>
+                                        <p className="feedback__text">Супер, мне все понравилось</p>
+                                    </div>
+                                    <div className="review">
+                                        <p className="user__name mb-0">Ivan Gogol</p>
+                                        <p className="user__email">jhon1@gmail.com</p>
+                                        <p className="feedback__text">Удовлетворительно</p>
+                                    </div>
+                                    <div className="review">
+                                        <p className="user__name mb-0">Vova Frunk</p>
+                                        <p className="user__email">jhon1@gmail.com</p>
+                                        <p className="feedback__text">Все очень плохо, аллергия</p>
+                                    </div>
+                                    <form className='form'>
+                                        <div className="form__group">
+                                            <input type="text" placeholder='Введите имя'/>
+                                        </div>
+                                        <div className="form__group">
+                                            <input type="text" placeholder='Введите имя'/>
+                                        </div>
+                                        <div className="form__group">
+                                            <textarea type="text" rows={5} placeholder='Введите имя'/>
+                                        </div>
+                                        <button type="submit" className="addCart__btn">
+                                            Отправить
+                                        </button>
+                                    </form>
+                                </div>
+                        }
+                    </Col>
+
+                    <Col lg="12" className="mb-5 mt-4">
+                        <h2>Похожие товары</h2>
+                    </Col>
+                    {
+                        relatedProduct.map((item) => (
+                            <Col key={item.id} lg='4' md='4' sm='6' xs='6' className='mb-4'>
+                                <ProductCart item={item}/>
+                            </Col>
+                        ))
+                    }
+                    ))}
+                </Row>
+            </Container>
+        </section>
+    </div>
     );
 };
 
